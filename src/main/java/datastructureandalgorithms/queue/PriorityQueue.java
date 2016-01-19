a49c6b7166720a70a6763286fd8c02430154f41b
rodbate <ssjiang@incardata.com.cn>
2016-01-19 16:06:10 +0800

    update

---
+++ src/main/java/datastructureandalgorithms/queue/PriorityQueue.java
@@ -0,0 +1,8 @@
+package datastructureandalgorithms.queue;
+
+/**
+ * Created by rod bate on 2016/1/19.
+ */
+public class PriorityQueue {
+
+}
---
+++ src/main/java/datastructureandalgorithms/queue/QueueWithoutItemsApp.java
@@ -0,0 +1,7 @@
+package datastructureandalgorithms.queue;
+
+/**
+ * Created by rodbate on 2016/1/19.
+ */
+public class QueueWithoutItemsApp {
+}
